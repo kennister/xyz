6e29f372700ff532a61c4b0bf0c8a4f2f2b29a5b
Kenny Lee <lizhaoming.86@gmail.com>
2011-09-15 20:18:45 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xyz::Application.config.secret_token = 'cabfb4a4caef9818eac77f01a3a799396d803dce3d35bd74b9fab27ed9c7147f9ae8aa5c7f9932a84b6e50e176fc87a254515c3125cfd44e655ba8f301a54583'
